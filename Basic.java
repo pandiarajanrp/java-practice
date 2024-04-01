01c6f7312325acfb2a4f689bbb8be515f900f5aa
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-04-01 16:48:07 -0600

    basic hello world
    basic hello world

---
+++ Basic.java
@@ -0,0 +1,13 @@
+class Basic {
+    public static void main(String[] args) {
+        int age = 34;
+        System.out.println("Hello World");
+        System.out.println("Age is " + age);
+        age = 23;
+        System.out.println("Age after changed " + age);
+
+        int myFirstNumber = 87;
+        int mySecondNumber = 98;
+        System.out.println("Addition " + (myFirstNumber + mySecondNumber));
+    }
+}
