ed28ba7c18c6efae64222c5e00af3fe8b8522d78
Pandiarajan Rajagopal <pandiarajan.job@gmail.com>
2024-04-04 20:29:27 -0600

    method type
    method type

--- KeywordsExpressions.java
+++ KeywordsExpressions.java
@@ -9,9 +9,15 @@ public class KeywordsExpressions {
 
         highScore++; //this is a statement
 
+        //create method
         printMessage();
+
+        //method with argument, void return
         printMessage("Welcome to Mexico");
 
+        //method with return type
+        int highScores = calculateHighScore(200);
+        System.out.println("The score is " + highScores);
     }
 
     public static void printMessage() {
@@ -20,5 +26,9 @@ public class KeywordsExpressions {
 
     public static void printMessage(String str) {
         System.out.println("This is method with arg " + str);
+    }
+
+    public static int calculateHighScore(int number) {
+        return number * 100;
     }
 }
